8184e45639a1c0cf15f40b7936c9d7176bd0bf93
ljh <luojionghuang@gmail.com>
2017-05-23 17:17:04 +0800

    rectification service

--- src/services/rectification.js
+++ src/services/rectification.js
@@ -12,12 +12,28 @@ const loadRectifications = (curPage) => fetch('http://127.0.0.1:9000/wsproject/r
  * 初始化整治信息
  * @param {Object} rectification 
  */
-const insertRectification = (rectification) => fetch('http://127.0.0.1:9000/wsproject/rectification/insertRectification', {
+const initRectification = (rectification) => fetch('http://127.0.0.1:9000/wsproject/rectification/initRectification', {
     checkDate: rectification.checkDate,
     enterpriseId: rectification.enterpriseId,
+})
+
+/**
+ * 修改整治信息
+ * @param {Object} rectification 
+ */
+const modifyRectification = (rectification) => fetch('http://127.0.0.1:9000/wsproject/rectification/modifyRectification', {
+    checkDate: rectification.checkDate,
+    checkPerson: rectification.checkPerson,
+    situation: rectification.situation,
+    method: rectification.method,
+    dutyUnit: rectification.dutyUnit,
+    dutyPerson: rectification.dutyPerson,
+    finishDate: rectification.finishDate,
+    remark: rectification.remark,
 })
 
 export {
     loadRectifications,
     insertRectification,
+    modifyRectification,
 }
